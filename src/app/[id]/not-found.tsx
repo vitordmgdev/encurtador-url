cbd84ad21fe0f8d7d5b0bf63c55db8192eca46de
Vitor <vitordossantosdomingues@gmail.com>
2025-08-14 19:24:51 -0300

    feat: Not found page fixed

--- src/app/[id]/not-found.tsx
+++ src/app/[id]/not-found.tsx
@@ -1,6 +1,6 @@
 const NotFoundPage = () => {
     return (
-        <div>
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
             <h1>Página não encontrada</h1>
             <p>Desculpe, mas a página que você está procurando não existe.</p>
         </div>
